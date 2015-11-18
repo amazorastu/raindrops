2458dbf15f2db66e48e796ecce38ac8f2b931e10
Pruto <525811388@qq.com>
2015-11-18 20:13:41 +0800

    添加了tableview的代码

--- Classes/Layers/AchievementLayer.cpp
+++ Classes/Layers/AchievementLayer.cpp
@@ -1,6 +1,7 @@
 #include "AchievementLayer.h"
 
 USING_NS_CC;
+USING_NS_CC_EXT;
 
 AchievementLayer::AchievementLayer()
 {
@@ -47,6 +48,23 @@ bool AchievementLayer::init()
 	this->addChild(label);
 
 
+
+	//创建一个talbleview 将datasource设置为当前的类的对象 tableview的显示区域大小为 300 * 300
+	TableView* tableView = TableView::create(this, CCSizeMake(300, 800));
+	//设置tableview为水平方向  ScrollView::Direction::VERTICAL 为垂直,ScrollView::Direction::HORIZONTAL 为水平
+	tableView->setDirection(ScrollView::Direction::VERTICAL);
+	//设置位置
+	tableView->setPosition(Vec2(Global::getWinSize().width / 2, Global::getWinSize().height / 3));
+	//设置代理对象
+	tableView->setDelegate(this);
+	// 填充顺序
+	tableView->setVerticalFillOrder(TableView::VerticalFillOrder::TOP_DOWN);
+	//添加tableview到当前layer
+	this->addChild(tableView);
+	//加载tableview
+	tableView->reloadData();
+
+
 	return true;
 }
 
@@ -71,4 +89,71 @@ void AchievementLayer::onKeyReleased(EventKeyboard::KeyCode keyCode, Event * pEv
 	{
 		Director::getInstance()->popScene();
 	}
+}
+
+
+//=======Table View=======
+
+//当cell被点击时调用  cell->getIdx()获取当前点击cell的编号
+void AchievementLayer::tableCellTouched(TableView* table, TableViewCell* cell)
+{
+	// 在输出窗口打印出，当前cell的编号
+	log("cell touched at index: %i", cell->getIdx());
+}
+
+//设置编号为 idx 的cell的大小  此处都为100*100
+Size AchievementLayer::tableCellSizeForIndex(TableView *table, ssize_t idx)
+{
+	return CCSizeMake(100, 100);
+}
+
+//由于tableview是动态获取数据的，该方法在初始化时会被调用一次，之后在每个隐藏的cell显示出来的时候都会调用
+TableViewCell* AchievementLayer::tableCellAtIndex(TableView *table, ssize_t idx)
+{
+	char string[5];
+	sprintf(string, "%u", idx);
+
+	// 在重用队列中获取一个cell 然后判断cell是否为空 不为空则创建一个新的
+	TableViewCell *cell = table->dequeueCell();
+
+	if (!cell) {
+		//创建一个新的cell
+		cell = TableViewCell::create();
+		//cell = new TableViewCell();
+		//加入到自动释放池中
+		//cell->autorelease();
+		//创建一个图片精灵
+		Sprite *sprite = Sprite::createWithSpriteFrameName("Drop1.png");
+		//设置精灵锚点为左下角
+		sprite->setAnchorPoint(Vec2::ZERO);
+		//设置精灵位置 相当于在cell中的相对位置
+		sprite->setPosition(Vec2(0, 0));
+		//将精灵加入到cell中
+		cell->addChild(sprite);
+		//创建一个标签
+		Label *label = Label::createWithTTF(string, "fonts/arial.ttf", 40.0f);
+		//设置标签相对cell的位置
+		label->setPosition(Vec2::ZERO);
+		//设置标签锚点为左下角
+		label->setAnchorPoint(Vec2::ZERO);
+		//为标签做一个标记,以便于在cell在重用队列中被取出来时，能够获取的该label并重置label信息
+		label->setTag(123);
+		//将标签加入到cell中
+		cell->addChild(label);
+	}
+	else
+	{
+		// 如果cell不为空,则根据tag获取到之前cell中存放的元素,并将元素信息重置
+		//获取当前cell中的label
+		Label *label = (Label*)cell->getChildByTag(123);
+		//重置label的相关信息
+		label->setString(string);
+	}
+	return cell;
+}
+
+//设置cell的个数 即一个tableview中包含了20个cell
+ssize_t AchievementLayer::numberOfCellsInTableView(TableView *table)
+{
+	return 20;
 }
--- Classes/Layers/AchievementLayer.h
+++ Classes/Layers/AchievementLayer.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #include "cocos2d.h"
+#include "cocos-ext.h"
 
 #include "Others/Global.h"
 #include "Others/TextBase.h"
 #include "Others/User.h"
 
-class AchievementLayer : public cocos2d::LayerColor
+class AchievementLayer : public cocos2d::LayerColor, public cocos2d::extension::TableViewDataSource, public cocos2d::extension::TableViewDelegate
 {
 public:
 	AchievementLayer();
@@ -18,8 +19,20 @@ public:
 	static cocos2d::Scene* createScene();
 	virtual void onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event * pEvent);
 
+
 public:
-	
+	virtual void scrollViewDidScroll(cocos2d::extension::ScrollView* view) {}
+	// 当tableview被放大或缩小时触发该方法  参数为当前tableview对象
+	virtual void scrollViewDidZoom(cocos2d::extension::ScrollView* view) {}
+	// 当cell被点击时调用该方法 参数为当前的tableview对象与被点击的cell对象
+	virtual void tableCellTouched(cocos2d::extension::TableView* table, cocos2d::extension::TableViewCell* cell);
+	// 设置tableview的Cell大小
+	virtual cocos2d::Size tableCellSizeForIndex(cocos2d::extension::TableView *table, ssize_t idx);
+	// 获取编号为idx的cell
+	virtual cocos2d::extension::TableViewCell* tableCellAtIndex(cocos2d::extension::TableView *table, ssize_t idx);
+	// 设置tableview中cell的个数
+	virtual ssize_t numberOfCellsInTableView(cocos2d::extension::TableView *table);
+
 
 private:
 	virtual void onEnterTransitionDidFinish();
